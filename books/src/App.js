633670dc1008a0626b94905938ae95bd561bd858
Yuliya_Shulgan <juliette7373@gmail.com>
2023-05-10 12:05:41 +0200

    feat

--- books/src/App.js
+++ books/src/App.js
@@ -1,4 +1,3 @@
-import './index.css';
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
@@ -6,15 +5,38 @@ import BookList from "./components/BookList";
 function App() {
     const [books, setBooks] = useState([]);
     const createBook = (title) => {
-        console.log('🚀 ~ createBook ~ title:', title);
+        const nextId = Math.floor(Math.random() * 9999);
+        const updatedBooks = [
+            ...books,
+            { id: nextId, title }
+        ];
+        setBooks(updatedBooks);
     };
-    const editBook = () => {};
-    const deleteBook = () => {};
+    const editBookById = ({id, title}) => {
+        const updatedBooks = books.map((book) => {
+            if (book.id === id) {
+                return {...book, title}
+            }
+            return book;
+        });
+        setBooks(updatedBooks);
+    };
+    const deleteBookById = (id) => {
+        const updatedBooks = books.filter((book) => {
+            return book.id.toString() !== id;
+        });
+        setBooks(updatedBooks);
+    };
 
     return(
-        <div>
+        <div className="app">
+            <h1>Reading list</h1>
+            <BookList 
+                books={books} 
+                onDelete={deleteBookById}
+                onEdit={editBookById}
+            />
             <BookCreate onCreate={createBook}/>
-            <BookList />
         </div>
     )
 }
--- books/src/components/BookEdit.js
+++ books/src/components/BookEdit.js
@@ -1,5 +1,30 @@
-function BookEdit() {
-    return <div>BookEdit</div>
+import { useState } from "react";
+
+function BookEdit({ onSubmit, book }) {
+    const [newTitle, setNewTitle] = useState(book.title);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onSubmit({title: newTitle, id: book.id});
+    }
+    const handleTitleChange = (event) => {
+        setNewTitle(event.target.value);
+    }
+
+    return(
+        <form className="book-edit" onSubmit={handleSubmit}>
+            <label className="title" htmlFor="title">Title</label>
+            <input 
+                id="title"
+                className="input" 
+                type="text"
+                value={newTitle}
+                onChange={handleTitleChange}
+            />
+            <button className="button is-primary" type="submit">
+                Save
+            </button>
+        </form>
+    )
 }
 
 export default BookEdit;
