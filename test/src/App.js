5d76e3045068deb536d7d057f5d48184b08f8d94
Yuliya_Shulgan <juliette7373@gmail.com>
2023-05-12 12:47:50 +0200

    feat

--- test/src/App.js
+++ test/src/App.js
@@ -13,44 +13,58 @@ function App() {
       } else {
         setBooks(response.data);
       }
-}
+  }
 
-  const createBook = async (title) => {
-    try {
-      const response = await axios.post('http://localhost:3001/books', {
-          title,
-      })
-      const updatedBooks = [
-          ...books,
-          response.data
-      ];
-      setBooks(updatedBooks);
-      return updatedBooks;
-} catch (e) {
-    console.log(e);
-   }
-   return null;
-};
+    const createBook = async (title) => {
+      try {
+        const response = await axios.post('http://localhost:3001/books', {
+            title,
+        })
+        const updatedBooks = [
+            ...books,
+            response.data
+        ];
+        setBooks(updatedBooks);
+        return updatedBooks;
+  } catch (e) {
+      console.log(e);
+    }
+    return null;
+  };
 
-useEffect(() => {
-  fetchBooks();
-}, []);
 
-  const editBookById = ({id, title}) => {
-    const updatedBooks = books.map((book) => {
+
+  const editBookById = async ({id, title}) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title
+    });
+    if (response.status !== 200) {
+      throw new Error('Data could not be updated!')
+    } else {
+      const updatedBooks = books.map((book) => {
         if (book.id === id) {
-            return {...book, title}
+            return {...book, ...response.data}
         }
         return book;
-    });
-    setBooks(updatedBooks);
-};
-const deleteBookById = (id) => {
-    const updatedBooks = books.filter((book) => {
+      });
+      setBooks(updatedBooks);
+    }
+  };
+  const deleteBookById = async (id) => {
+    const response = await axios.delete(`http://localhost:3001/books/${id}`);
+    if (response.status !== 200) {
+      throw new Error('Data could not be deleted!')
+    } else {
+      const updatedBooks = books.filter((book) => {
         return book.id.toString() !== id;
-    });
-    setBooks(updatedBooks);
-};
+      });
+      setBooks(updatedBooks);
+    }
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
 
   return (
     <div className="app">
